b675540f7542ac44e2816c12c8f46dd93d60454b
tekayakarim <36500892+tekayakarim@users.noreply.github.com>
2018-03-14 21:48:54 +0100

    Add files via upload

--- repertoire.h
+++ repertoire.h
@@ -6,6 +6,8 @@
  */
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
 
 /* Définition d'une fiche du répertoire */
 typedef struct {
@@ -23,15 +25,134 @@ typedef struct onglett{
 /* Définition du répertoire */
 typedef ongletptr repertoire[26];
 
-/* Déclaration des fonctions à définir */
-void afficheRepertoire(repertoire lerepertoire);
-void afficheOnglet(ongletptr longlet);
-void afficheFiche(ficheptr lafiche);
-void ajouterFiche(repertoire lerepertoire, ficheptr lafiche);
-void majuscule(ficheptr lafiche);
-void ajouterDansOnglet(ongletptr longlet, ficheptr lafiche);
-int avant(ficheptr fiche1, ficheptr fiche2);
-void repertoireVide(repertoire lerepertoire);
-void menu(repertoire lerepertoire);
-void freeRepertoire(repertoire lerepertoire);
-void rechercheTel(char *nom);
+/* Définition d'une fiche du répertoire */
+typedef struct {
+	char nom[21];
+	char prenom[21];
+	char tel[9];
+} fiche, *ficheptr;
+
+/* Définition d'un onglet */
+typedef struct onglett{
+	ficheptr lafiche;
+	struct onglett *suivante;
+} onglet, *ongletptr;
+
+/* Définition du répertoire */
+typedef ongletptr repertoire[26];
+
+//definition des fonctions
+void afficheRepertoire(repertoire lerepertoire) {
+	char c='A';
+	for(i=0;i<26;i++)
+	{
+		printf("%c",c+i);
+		afficheOnglet(lerepertoire[i]);
+	}
+}
+void afficheOnglet(ongletptr longlet) {
+	ongletptr p=longlet;
+	
+	do
+	{
+		afficheFiche(p->lafiche);
+		p=p->suivante;
+	}while(p->suivante!=NULL);
+}
+void afficheFiche(ficheptr lafiche) {
+	printf("le nom est %s le prenom %s le telephone %s\n ",lafiche->nom,lafiche->prenom,lafiche->tel );
+}
+void ajouterFiche(repertoire lerepertoire, ficheptr lafiche) {
+	char c='A';int i=0;
+	while(toupper(lafiche->nom[0]!=c)
+	{
+		c++;
+		i++;
+	}
+	majuscule(lafiche);
+	ajouterDansOnglet(lerepertoire[i]->longlet,lafiche);
+	
+}
+void majuscule(ficheptr lafiche) {
+	if(lafiche->nom[0]<'A' && lafiche->nom[0]<'Z')
+		lafiche->nom[0]=toupper(lafiche->nom[0]);
+}
+void ajouterDansOnglet(ongletptr longlet, ficheptr lafiche) {
+	longlet->lafiche=lafiche;
+	longlet=longlet->suivante;
+}
+int avant(ficheptr fiche1, ficheptr fiche2) {
+	if(fiche1->nom[0]<fiche2->nom[0])
+		return 1;
+	else
+		return 0;
+}
+void repertoireVide(repertoire lerepertoire) {
+	int i;
+	for(i=0;i<26;i++)
+	{
+		lerepertoire[i]=NULL;
+	}
+}
+void rechercheTel(char *nom){
+	i=0;c='A';
+	while(*nom[0]!=c)
+	{
+		i++;
+		c++;
+	}
+	p=lerepertoire[i]->longlet;
+	while(p->suivante!=NULL)
+	{
+		if(strcmp(p->lafiche->nom,*nom)==0)
+			
+			afficheFiche(p->lafiche)
+	}
+}
+void choix1() {
+	ajouterFiche(rep,rep->lafiche);
+}
+void choix2() {
+	afficheRepertoire(rep);
+}
+void choix3() {
+	ajouterFiche(rep,rep->lafiche);
+}
+void menu(repertoire lerepertoire) {
+	int choix;
+	do {
+		printf(
+				"=============================Gestion de répertoire=============================\n");
+		printf("\t\t1- choix1\n");
+		printf("\t\t2- choix2\n");
+		printf("\t\t3- choix3\n");
+		printf("\t\t4- Quitter\n");
+		printf(
+				"===============================================================================\n");
+		printf("\tEntrer votre choix(1-4):");
+		scanf("%d", &choix);
+		switch (choix) {
+		case 1:
+			choix1();
+			break;
+		case 2:
+			choix2();
+			break;
+		case 3:
+			choix3();
+			break;
+		case 4:
+			printf("Au revoir");
+			break;
+		default:
+			printf("Choix doit être dans [1,4]!");
+			break;
+		}
+	} while (choix != 5);
+
+}
+void freeRepertoire(repertoire lerepertoire) {
+	int i;
+	for(i=0;i<26;i++)
+		free(lerepertoire);
+}
